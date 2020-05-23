b1290e002f7c83e83dbe533fcfaaa6893fcc9580
solwininfotech <info@solwininfotech.com>
2020-05-23 09:36:01 +0530

    registration

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Our Team Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_Ourteam
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_Ourteam',
+    __DIR__
+);
